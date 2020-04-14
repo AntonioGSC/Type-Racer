450a333610a27c9ee9d3632f85bcfa29706b72d9
AntonioGSC <antoniogabrielagsc@gmail.com>
2020-04-13 23:42:49 -0300

    design and scoreboard

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ $(function(){
     atualizaTamanhoFrase();
     inicializaContador();
     inicializaCronometro();
+    compararDigiitacao();
     $('#botao-reiniciar').click(reiniciaJogo);
 });
 
@@ -33,24 +34,62 @@ function inicializaCronometro(){
             tempoRestante --;
             $('#tempo-digitacao').text(tempoRestante);
             if(tempoRestante < 1){
-                campo.attr('disabled', true);
                 clearInterval(cronometroID);
-                $('#botao-reiniciar').attr('disabled', false);
-                campo.addClass('campo-desativado');
+                finalizaJogo();
             }  
         }, 1000);
         console.log(tempoRestante);
     });
 }
 
+function finalizaJogo(){
+    campo.attr('disabled', true);
+    $('#botao-reiniciar').attr('disabled', false);
+    campo.toggleClass('campo-desativado');
+    inserePlacar();
+}
+
+function compararDigiitacao(){
+    let frase = $('.frase').text();
+    campo.on('input', function(){
+        let digitado = campo.val();
+        let comparavel = frase.substr(0, digitado.length);
+        console.log(digitado);
+        console.log(comparavel);
+        if(digitado == comparavel){
+            campo.addClass('campo-correto');
+            campo.removeClass('campo-errado');
+        }
+        else{
+            campo.addClass('campo-errado');
+            campo.removeClass('campo-correto');
+        }
+    });
+}
+
 function reiniciaJogo(){
         campo.removeClass('campo-desativado');
+        campo.removeClass('campo-errado');
+        campo.removeClass('campo.errado');
         campo.attr('disabled', false);
         campo.val('');
         $('#contador-caracteres').text(0);
         $('#contador-palavras').text(0);
         $('#tempo-digitacao').text(tempoInicial);
-        inicializaCronometro();
+        inicializaCronometro(); 
 }
+
+function inserePlacar(){
+    let tabela = $('.placar').find('tbody');
+    var usuario = 'Antonio';
+    let numPalavras = $('#contador-palavras').text();
+
+    let linha = '<tr>'+ 
+                        '<td>'+ usuario +'</td>'+
+                        '<td>'+ numPalavras +'</td>'+
+                '</tr>'
+    tabela.prepend(linha);
+}
+
 
 
